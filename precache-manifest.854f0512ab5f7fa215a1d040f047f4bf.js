e50bacb496f18b1db45ff56bae4daa4b2adc3088
Halid Macalangcom <halid_macalangcom@yahoo.com>
2020-04-05 22:03:10 +0800

    Updates

---
+++ precache-manifest.854f0512ab5f7fa215a1d040f047f4bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ea92e4d9964b17899f96af1d4d1d1c",
+    "url": "/vmhunter03/robofriends/index.html"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/vmhunter03/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2b3171178bd335b97928",
+    "url": "/vmhunter03/robofriends/static/css/main.d1bd9438.chunk.css"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/vmhunter03/robofriends/static/js/2.e636b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vmhunter03/robofriends/static/js/2.e636b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3171178bd335b97928",
+    "url": "/vmhunter03/robofriends/static/js/main.5799a681.chunk.js"
+  },
+  {
+    "revision": "c704d034e40ede176788",
+    "url": "/vmhunter03/robofriends/static/js/runtime-main.5334a58e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/vmhunter03/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
